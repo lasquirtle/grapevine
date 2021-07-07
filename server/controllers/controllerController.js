246f494eaa277d35839584399a0e7c435afc7066
lawrencehan650 <lawrencehan3@gmail.com>
2021-07-06 19:53:04 -0700

    adds error handling notes to controllers

--- server/controllers/controllerController.js
+++ server/controllers/controllerController.js
@@ -2,4 +2,14 @@ const db = require("../models/budgetModels");
 
 const controller = {};
 
+/*
+For handling errors:
+- return the following object as an argument to next
+.catch((err) => next({
+  log: `Error in controllerName.middlewareFunctionName: ${err}`, <- this is what logs in our console on the backend
+  message: '<Whatever the error is>' <- this is what gets sent back to the client
+  })
+)
+*/
+
 module.exports = controller;
