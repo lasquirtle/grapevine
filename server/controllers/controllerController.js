6929eb4068f39ac92496c6df35f3b5ff6398e4ef
lawrencehan650 <lawrencehan3@gmail.com>
2021-07-06 18:57:17 -0700

    creates boilerplate

---
+++ server/controllers/controllerController.js
@@ -0,0 +1,5 @@
+const db = require("../models/budgetModels");
+
+const controller = {};
+
+module.exports = controller;
--- server/server.js
+++ server/server.js
@@ -1,33 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const categoryRouter = require('./routes/categories.js')
-const itemRouter = require('./routes/items.js');
+const path = require("path");
+const router = require("./routes/router")
+const cookieParser = require("cookie-parser");
 
-
-
-app.get('/', (req, res) => {
-  return res.status(200).sendFile(path.resolve(__dirname, '../index.html'));
-})
-
-// define route handlers
-app.use('/categories', categoryRouter);
-app.use('/items', itemRouter);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.get("/", (req, res) => {
+  return res.status(200).sendFile(path.resolve(__dirname, "../index.html"));
+});
 
 // default error handler
 app.use((err, req, res, next) => {
   const defaultErr = {
-    log: 'Express error handler caught unknown middleware error',
+    log: "Express error handler caught unknown middleware error",
     status: 500,
-    message: { err: 'An error occurred' },
+    message: "An error occurred",
   };
   const errorObj = Object.assign({}, defaultErr, err);
   console.log(errorObj.log);
   return res.status(errorObj.status).json(errorObj.message);
 });
 
-
-
 app.listen(3000, () => {
-  console.log('Listening on port 3000...');
+  console.log("Listening on port 3000...");
 });
