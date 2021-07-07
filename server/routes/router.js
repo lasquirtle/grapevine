02eb4982c0737327296817aa877fb4f304165a79
Sean Yoo <seanyoo@Seans-Mac-mini.local>
2021-07-06 23:26:32 -0700

    added route handler

--- server/routes/router.js
+++ server/routes/router.js
@@ -3,3 +3,8 @@ const express = require("express");
 const router = express.Router();
 
 module.exports = router;
+
+router.get("/", (req, res) => {
+  return res.status(200).sendFile(path.resolve(__dirname, "../../index.html"));
+});
+
